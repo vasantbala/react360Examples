9b1ea4834ed5e7679afa4931dd1bd5a760b72b98
Vasant Balasubramanian <vasant.bala@gmail.com>
2019-12-22 12:01:58 -0600

    added container

--- react-spa/src/App.js
+++ react-spa/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
 } from 'react-router-dom';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 import WelcomeContainer from './modules/welcome/welcomeContainer';
 import MovieContainer from './modules/movie/movieContainer';
@@ -15,14 +17,22 @@ function App() {
   return (    
     <div>
     <HeaderContainer/>
-    <Switch>
-        <Route path='/movie'>
-          <MovieContainer/>
-        </Route>
-        <Route exact path='/'>
-          <WelcomeContainer/>
-        </Route>
-    </Switch>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="xlg">
+        <Typography component="div" style={{ backgroundColor: '#f5f5f5', height: '90vh' }}>
+          <Switch>
+            <Route path='/movie'>
+              <MovieContainer/>
+            </Route>
+            <Route exact path='/'>
+              <WelcomeContainer/>
+            </Route>
+          </Switch>
+        </Typography>
+      </Container>
+    </React.Fragment>
+
     </div>
   );
 }
