5777012144796547318384877acef617af785cbe
Vasant Balasubramanian <vasant.bala@gmail.com>
2019-12-21 20:50:27 -0600

    added material ui package

--- react-spa/src/modules/welcome/welcomeContainer.js
+++ react-spa/src/modules/welcome/welcomeContainer.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 function WelcomeContainer(){
     return(
+        <div>
         <p>Welcome!</p>
+        <Button variant="contained" color="primary">
+            Get Started
+        </Button>
+        </div>
+        
     );
 }
 
