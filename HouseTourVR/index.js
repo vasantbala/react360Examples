5bddf51b5f98c5046bf90792d24670705c445e8d
vasantbala <30379760+vasantbala@users.noreply.github.com>
2019-12-23 21:21:08 -0600

    HouseTour VR - with surface, buttons and dynamic bg change

---
+++ HouseTourVR/index.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Environment,
+  asset,
+  Text,
+  View,
+  VrButton
+} from 'react-360';
+import house from './data/houseData';
+
+export default class Buttons extends React.Component {
+
+  state = {
+    room: house.House.roomName,
+    info: house.House.info,
+    adjacentRooms: house.House.adjacentRooms
+  };
+
+  clickHandler(roomSelection){
+    this.setState({
+      room: house[`${roomSelection}`].roomName,
+      info: house[`${roomSelection}`].info,
+      adjacentRooms: house[`${roomSelection}`].adjacentRooms
+    });
+    Environment.setBackgroundImage(asset(`./360_${house[`${roomSelection}`].img}`));
+  }
+
+  createRoomButtons(adjacentRooms){
+    let rooms = adjacentRooms;
+    let buttons = [];
+    rooms.map(room => (
+      buttons.push(
+      <VrButton key={`${room}` + '-button'} onClick={ () => this.clickHandler(room)}>
+        <Text style={{backgroundColor: 'green'}}>{room}</Text>
+      </VrButton>)
+    ));
+
+    return buttons;
+  }
+
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.greetingBox}>
+        <Text style={styles.greeting}>
+            Room Selection
+          </Text>
+          <Text style={styles.greeting}>
+            { this.state.room }
+          </Text>
+          { this.createRoomButtons(this.state.adjacentRooms)}
+        </View>
+
+        <View style={styles.greetingBox}>
+          <Text>
+            { this.state.info }
+          </Text>
+        </View>
+      </View>
+    );
+  }
+};
+
+export class InfoPanel extends React.Component {
+
+  state = {
+    room: house.House.roomName,
+    info: house.House.info,
+    adjacentRooms: house.House.adjacentRooms
+  };
+
+  clickHandler(roomSelection){
+    this.setState({
+      room: house[`${roomSelection}`].roomName,
+      info: house[`${roomSelection}`].info,
+      adjacentRooms: house[`${roomSelection}`].adjacentRooms
+    });
+    Environment.setBackgroundImage(asset(`./360_${house[`${roomSelection}`].img}`));
+  }
+
+  createRoomButtons(adjacentRooms){
+    let rooms = adjacentRooms;
+    let buttons = [];
+    rooms.map(room => (
+      buttons.push(
+      <VrButton key={`${room}` + '-button'} onClick={ () => this.clickHandler(room)}>
+        <Text style={{backgroundColor: 'green'}}>{room}</Text>
+      </VrButton>)
+    ));
+
+    return buttons;
+  }
+
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.greetingBox}>
+        <Text style={styles.greeting}>
+            Room Selection
+          </Text>
+          <Text style={styles.greeting}>
+            { this.state.room }
+          </Text>
+          { this.createRoomButtons(this.state.adjacentRooms)}
+        </View>
+
+        <View style={styles.greetingBox}>
+          <Text>
+            { this.state.info }
+          </Text>
+        </View>
+      </View>
+    );
+  }
+};
+
+
+const styles = StyleSheet.create({
+  panel: {
+    // Fill the entire surface
+    width: 1000,
+    height: 600,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  greetingBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    borderColor: '#639dda',
+    borderWidth: 2,
+  }
+  
+});
+
+AppRegistry.registerComponent('Buttons', () => Buttons);
+AppRegistry.registerComponent('InfoPanel', () => InfoPanel);
+
